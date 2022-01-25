2f9723e2240d6d62a64d8e64f409cd90c0d0853a
chandru.R <chandru@changepond.com>
2022-01-25 12:27:19 +0530

    commited

---
+++ src/main/java/jarfilegeneratorpipeline/Sample.java
@@ -0,0 +1,10 @@
+package jarfilegeneratorpipeline;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("welcome to file list");
+	}
+
+}
